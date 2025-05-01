0ba5b3bad2b474ac81e162de87d88b688a71e02e
Kutukobra <nadzhiff@gmail.com>
2025-05-01 22:07:41 +0700

    CORS

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const cors = require('cors');
 
 app.use(cors({
     origin: 'https://cs9-nadzhif.vercel.app', 
-    methods: ['GET', 'POST', 'OPTIONS'],
+    methods: ['GET', 'POST', ],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://cs9-nadzhif.vercel.app"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
